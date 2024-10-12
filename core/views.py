53fcf8a9475a52c7b15dea5ea5f6ad5986337b65
Jdexurbano <dexterurbano25@gmail.com>
2024-10-12 13:54:12 +0800

    add page restriction

--- core/views.py
+++ core/views.py
@@ -1,34 +1,35 @@
 from .models import Task
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 # Create your views here.
 
-class TaskListView(ListView):
+class TaskListView(LoginRequiredMixin,ListView):
     model = Task
     template_name = 'core/index.html'
     context_object_name = 'tasks'
 
-class TaskDetailView(DetailView):
+class TaskDetailView(LoginRequiredMixin,DetailView):
     model = Task
     template_name = 'core/view-task.html'
     context_object_name = 'task'
 
-class CreateTaskView(CreateView):
+class CreateTaskView(LoginRequiredMixin,CreateView):
     model = Task
     template_name = 'core/add-task.html'
     fields = ('user','title','description')
     success_url = reverse_lazy('index')
 
-class UpdateTaskView(UpdateView):
+class UpdateTaskView(LoginRequiredMixin,UpdateView):
     model = Task
     template_name = 'core/update-task.html'
     fields = ('title','description','is_done')
     success_url = reverse_lazy('index')
     context_object_name = 'task'
 
-class DeleteTaskView(DeleteView):
+class DeleteTaskView(LoginRequiredMixin,DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('index')
